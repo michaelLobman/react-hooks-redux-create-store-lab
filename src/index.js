4f3de811230e9b3cde76f1d0e27c8e29aaf38bc8
Michael Lobman <michael.lobman@gmail.com>
2022-02-23 14:05:21 -0500

    commit tree b270a1fcd6401278bd6beb214d8b33a033ea7f4b

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
-  // write your createStore code here
+export function createStore(reducer) {
+  let state;
+  function getState(){
+    return state;
+  }
+
+  function dispatch(action){
+    state = reducer(state, action)
+    render();
+  }
+
+  return {
+    getState,
+    dispatch
+  }
+  
+  
+
 }
 
 export function candyReducer(state = [], action) {
@@ -12,6 +28,10 @@ export function candyReducer(state = [], action) {
       return state;
   }
 }
+
+
+let store = createStore(candyReducer);
+store.dispatch({ type: "@@INIT" });
 
 function render() {
   let container = document.getElementById("container");
